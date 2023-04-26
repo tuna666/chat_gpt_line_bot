aee6fca5d1bb28c0b0eb9ca7f27607a5767788bf
tuna <ev0.kpx@gmail.com>
2023-04-26 14:33:16 +0900

    エイリアスを追加

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -183,6 +183,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'LINEBot' => LINE\LINEBot::class,
     ])->toArray(),
 
 ];
